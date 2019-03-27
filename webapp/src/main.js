139f5c4bfc887bc5b7c199bf9e59c861c135349a
Wanchai Fuangmali <Wanchai@vstecs.co.th>
2019-03-27 15:12:13 +0700

    Webapp State01

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,9 +1,16 @@
-import Vue from 'vue'
+import Vue from "vue";
 import './plugins/vuetify'
-import App from './App.vue'
+import App from "./App.vue";
+import router from "./router/";
+import store from "./store";
+import "./registerServiceWorker";
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
